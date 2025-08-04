22652159f9fc94fead20de78faecdaacc4d6a387
Mounika-midde <mounika.midde33@gmail.com>
2025-08-04 14:23:49 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const { TodoRouter } = require('./routers/taskRoute'); // ✅ Correct file name
+
+const app = express();
+
+// Middleware
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods:["PUT","POST","GET", "DELETE"],
+  credentials: true
+}));
+app.use(express.json());
+
+// Routes
+app.use('/', TodoRouter);
+
+// Start the server
+app.listen(8080,"0.0.0.0", () => {
+  console.log('Server is running on port 8080');
+});
---
+++ server/routers/taskRoute.js
@@ -0,0 +1,32 @@
+// server/routes/taskRoute.js
+const { Router } = require('express');
+const { TodoRecord } = require('../records/todo.record');
+
+const TodoRouter = Router();
+
+TodoRouter.get('/', async (req, res) => {
+  const todosList = await TodoRecord.listAll();
+  res.send(todosList);
+});
+
+TodoRouter.post('/create', async (req, res) => {
+  const newTodo = new TodoRecord(req.body);
+  await newTodo.insert();
+  res.send('Values inserted successfully');
+});
+
+TodoRouter.delete('/:id', async (req, res) => {
+  const todo = await TodoRecord.getOne(req.params.id);
+  await todo.delete();
+  res.send('Deleted successfully');
+});
+
+TodoRouter.put('/update/:id', async (req, res) => {
+  const todo = await TodoRecord.getOne(req.params.id);
+  await todo.update(req.body.id, req.body.todo);
+  res.send('Updated successfully');
+});
+
+module.exports = {
+  TodoRouter,
+};
